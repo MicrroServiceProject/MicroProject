21548222f9a7ff6bf33e33f801483c22f8e46084
mejriahmednourallah <ahmednourallah.mejri@esprit.tn>
2025-04-08 08:35:27 +0100

    Id user

--- microproject/src/main/java/esprit/microproject/Entities/Instructor.java
+++ microproject/src/main/java/esprit/microproject/Entities/Instructor.java
@@ -16,7 +16,7 @@ public class Instructor implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+     private String userId;
     private String name;
     private String email;
 
--- microproject/src/main/java/esprit/microproject/Entities/Student.java
+++ microproject/src/main/java/esprit/microproject/Entities/Student.java
@@ -16,7 +16,7 @@ public class Student implements Serializable  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+     private String userId;
     private String name;
 
     @OneToMany(mappedBy = "student")
